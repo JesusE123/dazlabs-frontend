d5fa17291692fd7dc970822e9d74d90527a62e6a
JesusE123 <Jesusan0110@gmail.com>
2024-11-21 14:05:54 -0300

    chore:fix route

--- src/components/NewEmploye.tsx
+++ src/components/NewEmploye.tsx
@@ -88,7 +88,7 @@ const NewEmploye = () => {
           type="submit"
           className=" mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          <Link to="Employes">See Employes</Link>
+          <Link to="employes">See Employes</Link>
         </button>
       </div>
     </>
